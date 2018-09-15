0863f26707187f08dad00e53f72dd874b6a25971
NikosNtafopoulos <niklausblockchaindev@yahoo.com>
2018-09-16 00:03:09 +0300

    added nbaexample.py --> search and display all nba players on terminal

---
+++ web-scraping/nbaexample.py
@@ -0,0 +1,28 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+
+# path to phantomjs executable
+browser = webdriver.PhantomJS(
+    '/home/tsokis/phantomjs-2.1.1-linux-x86_64/bin/phantomjs')
+
+# url we want to target
+url = 'https://www.nba.com/players'
+
+# downloading the web page
+browser.get(url)
+
+soup = BeautifulSoup(browser.page_source, 'lxml')
+
+#section = soup.find('section', class_='block-league-content')
+#section = soup.find('section', class_='row nba-player-index__row')
+# getting all nba's players name
+section = soup.find('section', class_='row expanded ')
+
+for player in section.find_all('a'):
+    print player.text
+
+#print section
+
+#print browser.page_source
+
+browser.quit()
--- web-scraping/webScraping.py
+++ web-scraping/webScraping.py
@@ -3,14 +3,14 @@ import requests
 from csv import writer
 
 
-response = requests.get('http://sites name')
-soup = BeautifulSoup(response.text,'html.parser')
-posts = soup.find_all(class_='clear-fix')
-with open('posts.csv','w') as csv_file:
+response = requests.get('http://sites url')
+soup = BeautifulSoup(response.text, 'html.parser')
+posts = soup.find_all(class_='class you want to target')
+with open('posts.csv', 'w') as csv_file:
     csv_writer = writer(csv_file)
-    headersv= ['Title', 'Link', 'Date']
+    headersv = ['Title', 'Link', 'Date']
 
 for post in posts:
     print(post)
     #title = post.find(class_='post-title').get_text().replace('\n', '')
-    #print(title)
+    # print(title)
